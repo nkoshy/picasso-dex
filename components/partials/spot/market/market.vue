2a9836226c81dbbf84d190b4db758c7b09ed0f31
ankitmittal99 <ankit.mittal@primathon.in>
2021-07-27 08:29:41 +0530

    customize rate rolling window

--- components/partials/spot/market/market.vue
+++ components/partials/spot/market/market.vue
@@ -1,19 +1,17 @@
 <template>
   <v-panel v-if="market" class="h-full bg-light-purple rounded-md">
-    <div class="flex flex-wrap items-center mt-1 py-2 px-4 lg:py-1">
-      <div class="flex items-center mr-4 w-full md:w-auto lg:mb-0">
+    <div class="flex flex-wrap items-center mt-1 py-2 px-4 lg:py-1 mr-4 w-full md:w-auto lg:mb-0">
         <!-- <img
           :src="market.baseToken.icon"
           :alt="market.baseToken.name"
           class="w-6 h-6 mr-4"
         /> -->
-        <div class="">
+        <div class="pb-2">
           <p class="text-gray-100 font-semibold text-sm">{{ market.ticker }}</p>
           <p class="text-gray-500 text-xs">
             {{ market.baseToken.name }}
           </p>
         </div>
-      </div>
       <v-market-stats class="flex-1" />
     </div>
   </v-panel>

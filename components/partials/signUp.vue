d62775d8d38ce0b012178f4d9bedffb29c3732da
Ishigupta8 <ishi.gupta@primathon.in>
2021-09-15 16:42:28 +0530

    fix font-size of By clicking submit

--- components/partials/signUp.vue
+++ components/partials/signUp.vue
@@ -22,8 +22,8 @@
         class="mt-4"
         type="password"
         /> -->
-        <div class="text-xs font-sora text-white font-normal mt-4">
-          By clicking submit, you agree to our <span class="underline cursor-pointer" @click.stop="goPrivacy">Privacy Policy</span> and <span class="underline cursor-pointer" @click.stop="goTerms">Terms and Conditions</span>
+        <div class="text-sm font-sora text-white font-normal mt-4">
+          <span class="text-xs">By clicking submit</span>, you agree to our <span class="underline cursor-pointer" @click.stop="goPrivacy">Privacy Policy</span> and <span class="underline cursor-pointer" @click.stop="goTerms">Terms and Conditions</span>
         </div>
       <div class="w-full mx-auto mt-4 font-sora">
         <v-ui-button
@@ -41,7 +41,7 @@
 
 <script>
 import emailjs from 'emailjs-com';
-//import { email } from 'vee-validate/dist/rules';
+// import { email } from 'vee-validate/dist/rules';
 
 export default {
   data() {
